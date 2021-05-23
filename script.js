ad60255f8daf8b9f8a7b6374586b2c1f933043d6
ynkze <teyyiheng@gmail.com>
2021-05-23 06:34:52 -0700

    Basic functions done, keep track of running score and display winner

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
 let action = ["rock", "paper", "scissors"];
 let playerScore = 0;
+let computerScore = 0;
+
 
 
 //computer selects a random array number
@@ -23,12 +26,14 @@ function playRound(playerSelection, computerSelection) {
             return winMessage;
         }
         else if (computerSelection=="paper") {
+            computerScore++;
             return loseMessage;
         } 
     }
 
     else if (playerSelection=="paper"){
         if (computerSelection=="scissors"){
+            computerScore++;
             return loseMessage;
         }
         else if (computerSelection=="rock") {
@@ -43,29 +48,48 @@ function playRound(playerSelection, computerSelection) {
             return winMessage;
         }
         else if (computerSelection=="rock") {
+            computerScore++;
             return loseMessage;
         } 
     }   
 }
+//defining parameters
+let playerSelection = " ";
+const displayresult = document.querySelector('#result-display');
+const player_score = document.querySelector('#player_score');
+const computer_score = document.querySelector('#computer_score');
 
-function game(){
-    let tries = 5;
-    while (tries>0){
-        const computerSelection = computerPlay();
-        const playerAnswer = prompt("Rock, paper, or scissors?");
-        let playerSelection = playerAnswer.toLowerCase();
-
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Player score: ${playerScore}`);
-        tries--;
+//display the result and reset upon reaching 5
+function displayFunc () {
+    if (playerScore==5){
+        alert("You won with 5 points!");
+        playerScore = 0;
+        computerScore = 0;
     }
 
-    if (playerScore>=3){
-        alert("You win overall!")
+    if (computerScore==5){
+        alert("The computer won with 5 points!")
+        playerScore = 0;
+        computerScore = 0;
     }
 
-    else {
-        alert("You lose overall!")
-    }
 }
-game();
+
+// player click on button and it will return a value for player selection
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button)=> {
+    button.addEventListener('click', () => {
+        //take in player and computer input
+        playerSelection=button.value;
+        const computerSelection = computerPlay();
+        displayresult.textContent = playRound(playerSelection, computerSelection);
+        
+        // display running score
+        player_score.textContent = `Player: ${playerScore}`;
+        computer_score.textContent = `Computer: ${computerScore}`;
+        displayFunc();
+    });
+});
+
+
---
+++ scriptbackup.js
@@ -0,0 +1,71 @@
+let action = ["rock", "paper", "scissors"];
+let playerScore = 0;
+
+
+//computer selects a random array number
+function computerPlay() {
+    let result = Math.floor(Math.random()*action.length);
+    return action[result];
+}
+
+//execute 1 round
+function playRound(playerSelection, computerSelection) {
+    let winMessage = "You win!";
+    let loseMessage = "You lose!";
+
+    if (playerSelection==computerSelection){
+        return "It's a tie!";
+    }
+
+    else if (playerSelection=="rock"){
+        if (computerSelection=="scissors"){
+            playerScore++;
+            return winMessage;
+        }
+        else if (computerSelection=="paper") {
+            return loseMessage;
+        } 
+    }
+
+    else if (playerSelection=="paper"){
+        if (computerSelection=="scissors"){
+            return loseMessage;
+        }
+        else if (computerSelection=="rock") {
+            playerScore++;
+            return winMessage;
+        } 
+    }
+
+    else if (playerSelection=="scissors"){
+        if (computerSelection=="paper"){
+            playerScore++;
+            return winMessage;
+        }
+        else if (computerSelection=="rock") {
+            return loseMessage;
+        } 
+    }   
+}
+
+function game(){
+    let tries = 5;
+    while (tries>0){
+        const computerSelection = computerPlay();
+        const playerAnswer = prompt("Rock, paper, or scissors?");
+        let playerSelection = playerAnswer.toLowerCase();
+
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player score: ${playerScore}`);
+        tries--;
+    }
+
+    if (playerScore>=3){
+        alert("You win overall!")
+    }
+
+    else {
+        alert("You lose overall!")
+    }
+}
+game();
